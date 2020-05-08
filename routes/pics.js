f39e4180ce81a5181714554a8b514558023f48bc
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2020-05-08 23:09:25 +0200

    Added file upload functionality -pics.js - Added POST method for file upload

--- routes/pics.js
+++ routes/pics.js
@@ -1,24 +1,57 @@
 // IMPORTS
 const { Router } = require('express');
 const { db, storage } = require('./../firebase');
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
 
 // Init new router
 const router = new Router();
 
 
+router.use(fileUpload({
+    createParentPath: true
+}))
+
 
 
 // GET
 router.get('/:filename', async (req,res) => {
+    // Get buffer from storage bucket on firebase
     let pic = await storage.bucket().file(`hamster-pics/${req.params.filename}`).download();
+
+    // Convert into sendable binary data
     pic = Buffer.concat(pic);
+
+    // Send ok together with requested file
     res.status(200).contentType('jpeg').send(pic);
-})
+});
+
 
 
 
 // POST
+// Upload picture to hamster-pics on cloud storage
+router.post('/', async (req,res) => {
+    // set incoming file to pic
+    let pic = req.files.pic;
 
+    // Move the file to 
+    pic.mv('./uploads/' + pic.name)
+
+    console.log(`File upload: \nFilename: ${pic.name}\nMimetype: ${pic.mimetype}\nSize: ${pic.size}`);
+
+    // Upload file to cloud storage
+    await storage.bucket().upload(`./uploads/${pic.name}`, {destination: `hamster-pics/${pic.name}`});
+
+    // Delete temp file
+    fs.unlink(`./uploads/${pic.name}`, (err) => {
+        if (err) throw err;
+        console.log(`${pic.name} deleted from ./uploads`);
+    });
+
+    // Tell client that all is good
+    res.status(200).send({msg: `File ${pic.name} uploaded.`});
+})
 
 
 
