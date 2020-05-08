39e6f7a683fe9ac216018df03c9722c86ad44b3e
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2020-05-08 20:26:54 +0200

    -games.js - Added GET all games

--- routes/games.js
+++ routes/games.js
@@ -13,10 +13,27 @@ const router = new Router();
 
 // GET
 
+// Get all games as array
+router.get('/', async (req,res) => {
+    // Empty array for response
+    let gamesArr = [];
+
+    // Get all games from firebase
+    let games = await db
+    .collection('games')
+    .get();
+
+    // Push each game into array
+    games.forEach(game => gamesArr.push(game.data()));
+
+    // Respond with array of all games
+    res.status(200).send(gamesArr);
+});
 
 
 
 // POST
+
 // Add new game to games collection
 router.post('/', async (req,res) => {
     // Get new timestamp
