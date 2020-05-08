eac12b474b3000e0d7809646fa801dd2699000a5
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2020-05-08 10:46:09 +0200

    Routes set

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
+// IMPORTS
 const express = require('express');
 const { auth, db } = require('./firebase');
 const app = express();
 
+
+// Enable JSON formatting
 app.use(express.json());
+
+
+// STATIC ROUTES
 app.use('/assets', express.static('hamsters'));
 
 
+// ROUTE MODULES IMPORT
+const chartsRoute = require('./routes/charts');
+const gamesRoute = require('./routes/games');
+const hamstersRoute = require('./routes/hamsters');
+const statsRoute = require('./routes/stats');
+
+// ROUTES
+app.use('/charts', chartsRoute);
+app.use('/games', gamesRoute);
+app.use('/hamsters', hamstersRoute);
+app.use('/stats', statsRoute);
 
 
-
-
-
+// Start up server
 app.listen(3000, () => {
     console.log('Server is listening on port 3000...');
 })
---
+++ routes/charts.js
@@ -0,0 +1,33 @@
+// IMPORTS
+const { Router } = require('express');
+const { auth, db } = require('./../firebase');
+
+// Init new router
+const router = new Router();
+
+// GET
+
+
+
+
+
+
+// POST
+
+
+
+
+
+
+// PUT
+
+
+
+
+
+
+
+
+
+// EXPORTS
+module.exports = router;
