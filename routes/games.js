f859147f1d0a56ccd09bebf287f725c4cbea5b3a
Daniel Hölbling <29107418+iths-daniel-holbling@users.noreply.github.com>
2020-05-08 20:23:30 +0200

    -games.js - Added POST new game

--- routes/games.js
+++ routes/games.js
@@ -17,7 +17,23 @@ const router = new Router();
 
 
 // POST
+// Add new game to games collection
+router.post('/', async (req,res) => {
+    // Get new timestamp
+    let today = new Date();
 
+    // Add new game to firestore collection 'games' with timestamp and contestants
+    await db
+    .collection('games')
+    .doc()
+    .set({
+        timeStamp: today,
+        contestants: req.body
+    })
+
+    // Respond with "Game added." to client
+    res.status(201).send({msg: "Game added."})
+})
 
 
 
